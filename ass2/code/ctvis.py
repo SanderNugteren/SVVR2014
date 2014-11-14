39f82da22bed4c5f77246aa2904e4e39701c630b
Merel de Groot <merel_dg@bitbucket.org>
2014-11-14 14:35:35 +0100

    animation file added

--- ass2/code/ctvis.py
+++ ass2/code/ctvis.py
@@ -5,64 +5,84 @@
 import vtk
 import sys
 
-#print 'Initial contour value:', sys.argv[1] # 700 gives face, 1050 skull
+# Create timer for animation
+class vtkTimerCallback():
+	def __init__(self):
+		self.timer_count = 0
+ 
+	def execute(self,obj,event):
+		print self.timer_count
+		self.conFilter.SetValue(0, self.timer_count)
+		iren = obj
+		iren.GetRenderWindow().Render()
+		self.timer_count += 10
+		if self.timer_count > 3000:
+			iren.DestroyTimer()
 
-# SOURCE
-#read in images from ct scanner (94 images, 256x256 pixels)
-reader = vtk.vtkImageReader2()
-reader.SetFilePrefix("../data/slice")
-reader.SetDataExtent(0, 255, 0, 255, 1, 94)
-reader.SetDataOrigin(0.0, 0.0, 0.0)
-reader.SetDataScalarTypeToUnsignedShort()
-#reader.SetDataByteOrderToBigEndian()
-reader.SetDataSpacing(1,1,2) #data spacing
+def main():
+	# SOURCE
+	#read in images from ct scanner (94 images, 256x256 pixels)
+	reader = vtk.vtkImageReader2()
+	reader.SetFilePrefix("../data/slice")
+	reader.SetDataExtent(0, 255, 0, 255, 1, 94)
+	reader.SetDataOrigin(0.0, 0.0, 0.0)
+	reader.SetDataScalarTypeToUnsignedShort()
+	#reader.SetDataByteOrderToBigEndian()
+	reader.SetDataSpacing(1,1,2) #data spacing
 
-#update reader
-reader.UpdateWholeExtent()
-imageData = reader.GetOutput()
+	#update reader
+	reader.UpdateWholeExtent()
+	imageData = reader.GetOutput()
 
-# FILTER
-#get minimal and maximal values for isosurface
-minVal = imageData.GetScalarTypeMin() 
-maxVal = imageData.GetScalarTypeMax() 
+	# FILTER
+	#get minimal and maximal values for isosurface
+	minVal = imageData.GetScalarTypeMin() 
+	maxVal = imageData.GetScalarTypeMax() 
 
-#make isosurface
-conFilter = vtk.vtkContourFilter()
-conFilter.SetInput(imageData)
-conFilter.SetValue(0, float(sys.argv[1]))
+	#make isosurface
+	conFilter = vtk.vtkContourFilter()
+	conFilter.SetInput(imageData)
+	#conFilter.SetValue(0, float(sys.argv[1]))
+	cb = vtkTimerCallback()
+	cb.conFilter = conFilter
+	
+	# MAPPER
+	#make contour filter
+	pdm = vtk.vtkPolyDataMapper()
+	pdm.SetInput(conFilter.GetOutput())
+	#pdm.ScalarVisibilityOff() #option 6 
+	pdm.SetScalarRange(minVal, maxVal) #option 7
+	pdm.UseLookupTableScalarRangeOn() #option 7
 
-# MAPPER
-#make contour filter
-pdm = vtk.vtkPolyDataMapper()
-pdm.SetInput(conFilter.GetOutput())
-#pdm.ScalarVisibilityOff() #option 6 
-pdm.UseLookupTableScalarRangeOn() #option 7
-pdm.SetScalarRange(minVal, maxVal) #option 7
+	# ACTOR
+	actor = vtk.vtkLODActor()
+	actor.SetMapper(pdm)
+	#actor.GetProperty().SetColor(1, 1, 1) #option 6
 
-# ACTOR
-actor = vtk.vtkLODActor()
-actor.SetMapper(pdm)
-#actor.GetProperty().SetColor(1, 1, 1) #option 6
+	# RENDERER
+	ren = vtk.vtkRenderer()
+	#ren.SetBackground( 0.329412, 0.34902, 0.427451 ) #background colour
+	ren.AddActor(actor)
 
-# RENDERER
-ren = vtk.vtkRenderer()
-#ren.SetBackground( 0.329412, 0.34902, 0.427451 ) #background colour
-ren.AddActor(actor)
+	#camera
+	camera = vtk.vtkCamera()
+	camera.SetPosition(128,1000,200)
+	camera.SetFocalPoint(128,0,100)
+	ren.SetActiveCamera(camera)
 
-#camera
-camera = vtk.vtkCamera()
-camera.SetPosition(0,1000,-100)
-camera.SetFocalPoint(0,0,0)
-ren.SetActiveCamera(camera)
+	#renderwindow and render interactor
+	renwin = vtk.vtkRenderWindow()
+	renwin.AddRenderer(ren)	
+	iren = vtk.vtkRenderWindowInteractor()
+	iren.SetRenderWindow(renwin)
 
-#renderwindow and render interactor
-renwin = vtk.vtkRenderWindow()
-renwin.AddRenderer(ren)
-iren = vtk.vtkRenderWindowInteractor()
-iren.SetRenderWindow(renwin)
+	#initialize rendering with timer
+	renwin.Render()
+	iren.Initialize()
+	iren.AddObserver('TimerEvent', cb.execute)
+	timerId = iren.CreateRepeatingTimer(1)
 
-#start rendering
-iren.Initialize()
-renwin.Render()
-iren.Start()
+	#start rendering
+	iren.Start()
 
+main()
