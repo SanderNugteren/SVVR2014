8f92ad9a37fb2c75b5748e92eb89aeabb1584c44
unknown <sander.nugteren@gmail.com>
2014-11-20 19:12:05 +0100

    stream lines are generated along the circle edge. need to look at generating them inside the circle as well.

--- ass3/mix.py
+++ ass3/mix.py
@@ -20,28 +20,46 @@ conFilter = vtk.vtkContourFilter()
 conFilter.SetInput(imageData)
 conFilter.SetValue(0, 10)
 
+#make a flat cylinder to act as a source for streamlines by making a line,...
+lineSource1 = vtk.vtkLineSource()
+lineSource1.SetPoint1(1, 30, 30)
+lineSource1.SetPoint1(2, 0, 0)
+transL1 = vtk.vtkTransform()
+transL1.Translate(0, 30, 30)
+tf1 = vtk.vtkTransformPolyDataFilter()
+tf1.SetInputConnection(lineSource1.GetOutputPort())
+tf1.SetTransform(transL1)
+#then putting a tube around it
+cylinder1 = vtk.vtkTubeFilter()
+cylinder1.SetInputConnection(tf1.GetOutputPort())
+cylinder1.SetRadius(15)
+cylinder1.SetNumberOfSides(20)
+cylinder1.CappingOn()
+
 #make 'forward' vectorlines of the vectors (ie fluid1)
-streamline = vtk.vtkStreamLine()
-streamline.SetInputData(imageData)
-streamline.SetMaximumPropagationTime(200)
-streamline.SetIntegrationStepLength(.2)
-streamline.SetStepLength(0.001)
-streamline.SetNumberOfThreads(1)
-streamline.SetIntegrationDirectionToForward()
-streamline.VorticityOn()
+streamLine = vtk.vtkStreamLine()
+streamLine.SetInput(imageData)
+streamLine.SetSource(cylinder1.GetOutput())
+streamLine.SetMaximumPropagationTime(2000)
+streamLine.SetIntegrationStepLength(.2)
+streamLine.SetStepLength(0.1)
+streamLine.SetNumberOfThreads(100)
+streamLine.SetIntegrationDirectionToForward()
+streamLine.VorticityOn()
+#streamLine.SetStartPosition(0, 25, 25)	
 
 #make 'backward' vectorlines of the vectors (ie fluid2)
 streamFilter2 = vtk.vtkStreamTracer()
 streamFilter2.SetInput(imageData)
 streamFilter2.SetIntegrationDirectionToBackward()
 
-"""
 #make tubes around the streams for visibility
 streamTube1 = vtk.vtkTubeFilter()
-streamTube1.SetInputConnection(streamFilter1.GetOutput())
-streamTube1.SetRadius(0.02)
+streamTube1.SetInput(streamLine.GetOutput())
+streamTube1.SetRadius(0.5)
 streamTube1.SetNumberOfSides(12)
 
+"""
 streamTube2 = vtk.vtkTubeFilter()
 streamTube2.SetInputConnection(streamFilter2.GetOutput())
 streamTube2.SetRadius(0.02)
@@ -53,10 +71,12 @@ pdm1 = vtk.vtkPolyDataMapper()
 pdm1.SetInput(conFilter.GetOutput())
 
 pdm2 = vtk.vtkPolyDataMapper()
-pdm2.SetInput(streamLine.GetOutput())
+pdm2.SetInput(streamTube1.GetOutput())
 
 pdm3 = vtk.vtkPolyDataMapper()
-pdm3.SetInput(streamFilter2.GetOutput())
+#pdm3.SetInput(streamFilter2.GetOutput())
+pdm3.SetInput(cylinder1.GetOutput())
+
 
 # 3 ACTORS
 actor1 = vtk.vtkLODActor()
