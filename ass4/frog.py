5cc9248cd44df3b6673f957815ecef9b6fe187b0
unknown <sander.nugteren@gmail.com>
2014-12-05 15:13:44 +0100

    froggy has colours

--- ass4/frog.py
+++ ass4/frog.py
@@ -8,46 +8,73 @@ import sys
 # SOURCE
 #read in images from ct scanner (94 images, 256x256 pixels)
 reader = vtk.vtkImageReader2()
-reader.SetFilePrefix("../WholeFrog/")
-reader.SetDataExtent(0, 255, 0, 255, 1, 94)
-reader.SetDataOrigin(0.0, 0.0, 0.0)
-reader.SetDataScalarTypeToUnsignedShort()
-#reader.SetDataByteOrderToBigEndian()
-reader.SetDataSpacing(1,1,2) #data spacing
+reader.SetFilePrefix("./WholeFrog/frogTissue.")
+reader.SetFilePattern("%s%03d.raw")
+reader.SetDataExtent(0, 499, 0, 469, 1, 136)
+reader.SetDataOrigin(1.0, 1.0, 1.0)
+reader.SetDataScalarTypeToUnsignedChar()
+reader.SetDataSpacing(1,1,1.5) #data spacing
 
 #update reader
 reader.UpdateWholeExtent()
 imageData = reader.GetOutput()
 
-# FILTER
-#get minimal and maximal values for isosurface
-#minVal = imageData.GetScalarTypeMin() 
-#maxVal = imageData.GetScalarTypeMax() 
+organList = ['', 'blood', 'brain', 'duodenum', 'eye retina', 'eye white', 'heart', 'ileum', 'kidney', 'large intestine', 'liver', 'lung', 'nerve', 'skeleton', 'spleen', 'stomach']
 
-#make isosurface
-conFilter = vtk.vtkContourFilter()
-conFilter.SetInput(imageData)
-#conFilter.SetValue(0, float(sys.argv[1]))
-conFilter.SetValue(0, 700)
-conFilter.SetValue(1, 3000)
+color = {}
+color['blood'] = (1,0,0)
+color['brain'] = (0,0,1)
+color['duodenum'] = (0,1,0)
+color['eye retina'] = (1,0,1)
+color['eye white'] = (0.5,0,0.5)
+color['heart'] = (0,0,0)
+color['ileum'] = (0.45,0.3,0.2)
+color['kidney'] = (1,0.5,0.3)
+color['large intestine'] = (1,1,0)
+color['liver'] = (0,1,1)
+color['lung'] = (0,0.5,0)
+color['nerve'] = (1,0.5,1)
+color['skeleton'] = (1,1,1)
+color['spleen'] = (0.3,0.5,0.3)
+color['stomach'] = (0.2,0.2,0.7)
 
-# MAPPER
-#make contour filter
-pdm = vtk.vtkPolyDataMapper()
-pdm.SetInput(conFilter.GetOutput())
-#pdm.ScalarVisibilityOff() #color: option 6 
-pdm.SetScalarRange(0, 3000) #color: option 7
+actorList = []
 
-# ACTOR
-actor = vtk.vtkLODActor()
-actor.SetMapper(pdm)
-#actor.GetProperty().SetColor(1, 1, 1) #color: option 6
+for i in xrange(1,15):
+	#make isosurface
+	thresholdFilter = vtk.vtkThreshold()
+	thresholdFilter.SetInput(imageData)
+	thresholdFilter.ThresholdBetween(i, i)
+
+	# MAPPER
+	#make contour filter
+	dsm = vtk.vtkDataSetMapper ()
+	dsm.SetInput(thresholdFilter.GetOutput())
+	dsm.ScalarVisibilityOff() #color: option 6 
+	#dsm.SetScalarRange(0, 3000) #color: option 7
+
+	# ACTOR
+	actor = vtk.vtkLODActor()
+	actor.SetMapper(dsm)
+	organCol = color[organList[i]]
+	actor.GetProperty().SetColor(organCol) #color: option 6
+	actorList.append(actor)
 
 # RENDERER
 ren = vtk.vtkRenderer()
-#ren.SetBackground( 0.329412, 0.34902, 0.427451 ) 
-ren.AddActor(actor)
+ren.SetBackground( 0.329412, 0.34902, 0.427451 ) 
 
+renderList = range(1,15)
+for i in renderList:
+	ren.AddActor(actorList[i-1])
+
+legend = vtk.vtkLegendBoxActor()
+for i in renderList:
+	legend.SetEntryString(i, organList[i])
+	legend.SetEntryColor(i, color[organList[i]])
+	
+ren.AddActor(legend)
+	
 #camera
 #camera = vtk.vtkCamera()
 #camera.SetPosition(128,1000,200)
