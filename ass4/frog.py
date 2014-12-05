8961e446f835f1e1ab9387f454c4edd93cacaf66
Merel de Groot <merel_dg@bitbucket.org>
2014-12-05 16:13:41 +0100

    froggy has a working legend with renewed colours

--- ass4/frog.py
+++ ass4/frog.py
@@ -14,33 +14,37 @@ reader.SetDataExtent(0, 499, 0, 469, 1, 136)
 reader.SetDataOrigin(1.0, 1.0, 1.0)
 reader.SetDataScalarTypeToUnsignedChar()
 reader.SetDataSpacing(1,1,1.5) #data spacing
-
-#update reader
 reader.UpdateWholeExtent()
 imageData = reader.GetOutput()
 
+# LIBRARY
 organList = ['', 'blood', 'brain', 'duodenum', 'eye retina', 'eye white', 'heart', 'ileum', 'kidney', 'large intestine', 'liver', 'lung', 'nerve', 'skeleton', 'spleen', 'stomach']
 
+# setting the colour for each tissue
 color = {}
-color['blood'] = (1,0,0)
-color['brain'] = (0,0,1)
-color['duodenum'] = (0,1,0)
-color['eye retina'] = (1,0,1)
-color['eye white'] = (0.5,0,0.5)
-color['heart'] = (0,0,0)
-color['ileum'] = (0.45,0.3,0.2)
-color['kidney'] = (1,0.5,0.3)
-color['large intestine'] = (1,1,0)
-color['liver'] = (0,1,1)
-color['lung'] = (0,0.5,0)
-color['nerve'] = (1,0.5,1)
-color['skeleton'] = (1,1,1)
-color['spleen'] = (0.3,0.5,0.3)
-color['stomach'] = (0.2,0.2,0.7)
+color['blood'] = (1,0,0)        #red
+color['skeleton'] = (1,1,1)     #white
+color['eye retina'] = (0.8,0,0.8) #pink
+color['eye white'] = (1,0,1) #light pink
+color['nerve'] = (0.5,0,0.5)    #purple
+color['brain'] = (0.2,0,0.2)        #blue (bright)
+color['heart'] = (0,0,0)        #black
+color['lung'] = (0,1,0)         #blue (dark)
 
+color['liver'] = (0.45,0.3,0.2) #brown
+color['kidney'] = (1,0.5,0.3)   #orange
+
+color['duodenum'] = (0.3,0.3,0) #
+color['ileum'] = (0.6, 0.6, 0)  #
+color['large intestine'] = (1,1,0) #yellow
+
+color['spleen'] = (0.3,0.5,0.3) #green
+color['stomach'] = (0.2,0.2,0.7) #dark blue
+
+# ACTOR PER THRESHOLD FILTER
 actorList = []
 
-for i in xrange(1,15):
+for i in xrange(1,len(organList)):
 	#make isosurface
 	thresholdFilter = vtk.vtkThreshold()
 	thresholdFilter.SetInput(imageData)
@@ -64,31 +68,25 @@ for i in xrange(1,15):
 ren = vtk.vtkRenderer()
 ren.SetBackground( 0.329412, 0.34902, 0.427451 ) 
 
-renderList = range(1,15)
+renderList = range(1,len(organList))
 for i in renderList:
 	ren.AddActor(actorList[i-1])
 
+# LEGEND
 legend = vtk.vtkLegendBoxActor()
 legend.SetNumberOfEntries(len(renderList))
 #legendBox = vtk.vtkCubeSource()
 #legendBox.Update()
 for i in renderList:
-	legend.SetEntryString(i, organList[i])
-	legend.SetEntryColor(i, color[organList[i]])
+    legend.SetEntryString(i-1, organList[i])
+    legend.SetEntryColor(i-1, color[organList[i]])
 
 legend.GetPositionCoordinate().SetCoordinateSystemToView()
 legend.GetPositionCoordinate().SetValue(.5, -0.99)
 legend.GetPosition2Coordinate().SetCoordinateSystemToView()
 legend.GetPosition2Coordinate().SetValue(0.99, -0.25)
-	
 ren.AddActor(legend)
 	
-#camera
-#camera = vtk.vtkCamera()
-#camera.SetPosition(128,1000,200)
-#camera.SetFocalPoint(128,0,100)
-#ren.SetActiveCamera(camera)
-
 #renderwindow and render interactor
 renwin = vtk.vtkRenderWindow()
 renwin.AddRenderer(ren)	
